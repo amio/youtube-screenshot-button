588570cd8de9c9272b11a41cc884c10cee5a9b41
Amio • 晋晓炜 <amio.cn@gmail.com>
2016-08-23 16:08:00 +0800

    Refine button size for full screen mode.

--- youtube-screenshot.user.js
+++ youtube-screenshot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Youtube Screenshot Button
 // @author      Amio
-// @version     1.0.1
+// @version     1.0.2
 // @description Adds a button that lets you take screenshot.
 // @homepageURL https://github.com/amio/youtube-screenshot-button
 // @match       http://www.youtube.com/*
@@ -11,17 +11,17 @@
 // ==/UserScript==
 
 (function () {
-  var icon = '<svg style="padding:13px 11px;" width="16" height="10" viewBox="0 0 16 10" ' +
+  var icon = '<svg width="16" height="10" viewBox="0 0 16 10" ' +
     'xmlns="http://www.w3.org/2000/svg"><title>ytp-svg-transition-10</title><path d="M16 ' +
     '0v10H0V0h16zM2 2h12v6H2V2zm0 6h3v2H2V8zm9-8h3v2h-3V0z" fill="#FFF" fill-rule="evenodd"></path></svg>'
-  var iconBig = '<svg style="padding:11px 8px" width="20" height="14" viewBox="0 0 20 14" ' +
+  var iconBig = '<svg width="20" height="14" viewBox="0 0 20 14" ' +
     'xmlns="http://www.w3.org/2000/svg"><title>ytp-svg-transition-11</title><path d="M20 ' +
     '0v14H0V0h20zM2 2h16v10H2V2zm13-2h3v2h-3V0zM2 12h3v2H2v-2z" fill="#FFF" fill-rule="evenodd"/></svg>'
 
   var video = document.querySelector('.html5-main-video')
   var controls = document.querySelector('.ytp-right-controls')
   var existingButton = document.querySelector('.ytp-screenshot')
-  
+
   if (existingButton) {
     console.info('Screenshot button already exists.')
     return
@@ -44,8 +44,12 @@
   }
 
   if (video && controls) {
-    var buttonHTML = `<button class="ytp-button ytp-screenshot" title="Screenshot">${icon}</button>`
-    controls.insertAdjacentHTML('afterbegin', buttonHTML)
+    var styles = `<style>
+                    .ytp-screenshot { vertical-align: top; text-align:center; }
+                    .ytp-screenshot svg { height: 25%; width: 50% }
+                  </style>`
+    var buttonHTML = `<button class="ytp-button ytp-screenshot" style="${styles}" title="Screenshot">${icon}</button>`
+    controls.insertAdjacentHTML('afterbegin', buttonHTML + styles)
     controls.firstChild.addEventListener('click', function () {
       var image = createScreenshotFromVideo(video)
       window.open(image)
